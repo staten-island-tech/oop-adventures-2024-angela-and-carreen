862f7694592eb518173a48ce0abd0f4b75fbdaca
CarreenWu <carreenw@nycstudents.net>
2024-12-05 09:26:08 -0500

    yayayaya

--- monster.py
+++ monster.py
@@ -1,35 +1,49 @@
+import random
+
 class Monster: 
-    def __init__(self, name, hp, attack, defense, drops):
+    def __init__(self, name, hp, attack, drops):
         self.name=name
         self.hp=hp
         self.attack=attack
-        self.defense=defense
         self.drops=drops
     
     def attacking(self, target):
-        print(f"{self.name} attacks {target.name} target take {self.attack} damage!")
-        target.take_damage(self.attack)
+        if self.hp>0:
+            print(f"{self.name} attacks {target.name} target take {self.attack} damage!")
+            target.take_damage(self.attack)
         
     def take_damage(self, damage):
         self.hp -= damage
-        if self.hp<0:
+        if self.hp<=0:
             self.die()
         else:
-            print(f"{self.name}now has {self.hp} HP left")
+            print(f"{self.name} now has {self.hp} HP left")
     
     def die(self):
-        print(f"{self.name}is now dead")
+        if self.hp<=0:
+            print(f"{self.name}is now dead")
+            self.drop_item()
     
     def alive(self):
         return self.hp>0
+        print (f"{self.name} takes {damage} damage, {self.hp} hp remaining")
     
     def drop_item(self):
-        if self.die():
-            
+        if self.drops:
+            print (f"{self.name}drops these")
+            num_drops = random.randint(1, len(self.drops))
+            for _ in range(num_drops):
+                dropped_item = random.choice(list(self.drops))
+                print(f"- {dropped_item}")
+        else:
+            print("no drops availble")
 
 
+    
+    
+    
 
-
-
-skeleton = Monster("skeleton", 30, 10, 0, {"bones"})
+werewolf = Monster("Werewolf", 100, 50, {"werewolf fur", "werewolf claws", "gold coins"})
+goblin = Monster("Goblin", 50, 20, {"goblin skin", "wooden shield", "gold coin"})
+skeleton = Monster("skeleton", 30, 10, {"bones", "rusty sword", "gold coin"})
 
