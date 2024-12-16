992ee8a2c953fc3c3215448ad44d78fab65458f0
Angelan0 <angelan29@nycstudents.net>
2024-12-16 09:25:44 -0500

    6

--- classes/knight.py
+++ classes/knight.py
@@ -1,4 +1,4 @@
-class knight:
+class Knight:
     def __init__(self, name, health, mana, defense, attacks):
         self.name = name
         self.health = health
@@ -6,28 +6,33 @@ class knight:
         self.mana = mana
         self.attacks = attacks
         self.attack_type = {
-            1: {"name": "Slash", "damage": 10, "mana cost": 0},
-            2: {"name": "Charge Attack", "damage": 15, "mana cost": 10},
-            3: {"name": "Power Slash", "damage": 30, "mana cost": 15}}
+            1: {"name": "Slash", "damage": 10, "mana_cost": 0},
+            2: {"name": "Charge Attack", "damage": 15, "mana_cost": 10},
+            3: {"name": "Power Slash", "damage": 30, "mana_cost": 15}}
+
     def attacking(self, target, attack_choice):
         if attack_choice not in self.attack_type:
-            print(f"{attack_choice} is not a valid arrow choice")
+            print(f"{attack_choice} is not a valid attack choice.")
             return
 
-        attack = self.attack_types[attack_choice]
+        attack = self.attack_type[attack_choice]
 
-        if self.arrows > 0:
-            print(f"{self.name} attacks {target.name} with {attack['name']}")
-            damage = arrow["damage"]
-            target.take_damage(damage)
-            if attack["mana_cost"] > 0:
-                if self.mana >= attack["mana_cost"]:
-                    self.mana -= attack["mana_cost"]
-                    print(f"{self.name} used {attack['name']} consuming {attack['mana_cost']} mana.")
-                else:
-                    print(f"{self.name} doesn't have enough mana for {attack['name']}!")
-        else:
-            print(f"{self.name} can't attack anymore")
+        if self.attacks <= 0:
+            print(f"{self.name} can't attack anymore!")
+            return
+
+        if attack["mana_cost"] > 0:
+            if self.mana >= attack["mana_cost"]:
+                self.mana -= attack["mana_cost"]
+                print(f"{self.name} uses {attack['name']} consuming {attack['mana_cost']} mana.")
+            else:
+                print(f"{self.name} doesn't have enough mana for {attack['name']}!")
+                return
+
+        print(f"{self.name} attacks {target.name} with {attack['name']}!")
+        damage = attack["damage"]
+        target.take_damage(damage)
+        self.attacks -= 1
 
     def take_damage(self, damage):
         reduced_damage = max(damage - self.defense, 0)
@@ -38,4 +43,4 @@ class knight:
             print(f"{self.name} takes {reduced_damage} damage, {self.health} health remaining.")
 
     def __str__(self):
-        return f"knight {self.name}: Health = {self.health}, Mana = {self.mana}, Defense = {self.defense}"
+        return f"Knight {self.name}: Health = {self.health}, Mana = {self.mana}, Defense = {self.defense}, Attacks = {self.attacks}"
---
+++ npcs.py
@@ -0,0 +1,11 @@
+class npc:
+    def __init__(self, name, occupation, money, inventory):
+        self.name = name
+        self.occupation = occupation
+        self.money = money
+        self.inventory = inventory
+
+    def 
+
+
+#store clerk, traders, inn keeper
