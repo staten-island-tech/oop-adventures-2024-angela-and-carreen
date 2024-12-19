f6652b5a50e8c535b9a31ab804819316f04f0bb4
CarreenWu <carreenw@nycstudents.net>
2024-12-19 09:00:15 -0500

    gfcvh /

---
+++ items.py
@@ -0,0 +1,60 @@
+class Items():
+    def __init__(self, name, price, durability, effect, effect_duration):
+        self.name=name
+        self.price=price
+        self.durability=durability
+        self.effect=effect
+        self.effect_duration=effect_duration
+        self.is_active=False
+
+    def activate_effect(self):
+        if self.durability>=0:
+            self.durability -=1
+            return f"Durability is now {self.durability}"
+        else:
+            return f"{self.name} is now destroyed"
+
+    def apply_effect(self):
+        if self.duration > 0:
+            self.is_active=True
+            print(f"The effect of {self.name} is active for {self.duration}")
+        else:                            
+            print(f"{self.name} ability is N/A")
+
+    def update_duration(self):
+        if self.is_active:
+            self.duration -=1
+            print(f"{self.name}'s effect is now over")
+            if self.duration <= 0:
+                self.expire_effect()  
+
+    def expire_effect(self):
+        self.is_active=False
+        print(f"the effect of {self.name} is now over")
+
+    def repair(self, amount):
+        self.durability += amount
+        return f"{self.name} has been fixed. durability has become {self.durability}"
+
+class Weapons(Items):
+    def __init__(self, name, price, durability, effect, effect_duration, rarity):
+        super().__init__(name, price, durability, effect, effect_duration)
+        self.rarity=rarity
+        self.is_active=False
+
+
+lamp = Items("Lamp", 5, 100, "light", 10)
+Map = Items("Map", 3, 50, "N/A", "N/A")
+Gold_coin = Items("Gold Coin", 1, 1, "N/A", "N/A")
+healing_potion = Items("Healing Potion", 10, 1, "heal", 5)
+exploding_potion = Items("Exploding Potion", 10, 1, "explosion", 5)
+invisibility_potion = Items("Invisibility Potions", 10, 1, "invisibility", 5)
+invincibility_potion = Items("invincibility Potions", 10, 1, "invincibility", 5)
+laxitive = Items("Laxitive", 15, 1, 'diarrhea', 10)
+shoe = Items("shoes", 5, 100, "N/A", "N/A")
+sword = Weapons("Sword", 15, 50, "Common", "N/A", "N/A")
+shield = Weapons("Shield", 10, 75, "Common", "N/A", "N/A")
+bow = Weapons("Bow", 15, 100, "Common", "N/A", "N/A")
+arrows = Weapons("Arrows", 1, 5, "Common", "N/A", "N/A")
+staff = Weapons("Staff", 10, 100,  "Common", "N/A", "N/A")
+gun = Weapons("Gun", 1000, 10, "Legendary", "Bullet", 1)
---
+++ monster.py
@@ -0,0 +1,46 @@
+import random
+
+class Monster: 
+    def __init__(self, name, hp, attack, drops):
+        self.name=name
+        self.hp=hp
+        self.attack=attack
+        self.drops=drops
+    
+    def attacking(self, target):
+        if self.hp>0:
+            print(f"{self.name} attacks {target.name} target take {self.attack} damage!")
+            target.take_damage(self.attack)
+        
+    def take_damage(self, damage):
+        self.hp -= damage
+        if self.hp<=0:
+            self.die()
+        else:
+            print(f"{self.name} now has {self.hp} HP left")
+    
+    def die(self):
+        if self.hp<=0:
+            print(f"{self.name} is now dead")
+            self.drop_item()
+    
+    def alive(self):
+        return self.hp>0
+        print (f"{self.name} takes {damage} damage, {self.hp} hp remaining")
+    
+    def drop_item(self):
+        if self.drops:
+            print (f"{self.name} drops these")
+            num_drops = random.randint(1, len(self.drops))
+            for _ in range(num_drops):
+                dropped_item = random.choice(list(self.drops))
+                print(f"- {dropped_item}")
+        else:
+            print("no drops availble")
+
+
+
+werewolf = Monster("Werewolf", 100, 50, {"werewolf fur", "werewolf claws", "55 gold coins"})
+goblin = Monster("Goblin", 50, 20, {"goblin skin", "wooden shield", "5 gold coins"})
+skeleton = Monster("skeleton", 30, 10, {"bones", "rusty sword", "3 gold coins"})
+dragon = Monster("Dragon", 150, 80, {"dragon scales", "dragon tooth", "dragon heart", '100 gold coins'})
