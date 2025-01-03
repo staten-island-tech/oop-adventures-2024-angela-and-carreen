fe3b7e3bcacb16e4397a7049d5bb7eb08f1ac288
Angelan0 <angelan29@nycstudents.net>
2025-01-03 09:04:40 -0500

    rx

---
+++ main.py
@@ -0,0 +1,10 @@
+import monster
+import items
+import user
+import npcs
+import json
+
+def game_set_up():
+    def __init__(self, name)
+    self.name = name
+        print (f"Hello {player.name}")
