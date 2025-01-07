097c6b4aafe212763b3655c719d71463853f4e60
Angelan0 <angelan29@nycstudents.net>
2025-01-07 09:24:30 -0500

    day 15?

--- monster.py
+++ monster.py
@@ -39,41 +39,15 @@ class Monster:
                 print("Invalid action. Try again.")
         if player.health <= 0:
             print("You have been defeated...")
-            
+
 werewolf = Monster("Werewolf", 100, 25, {"werewolf fur", "werewolf claws", "55 gold coins"})
 goblin = Monster("Goblin", 50, 5, {"goblin skin", "wooden shield", "5 gold coins"})
 skeleton = Monster("Skeleton", 30, 10, {"bones", "rusty sword", "3 gold coins"})
 slime = Monster("Slime", 15, 1, {"1 gold coin"})
 dragon = Monster("Dragon", 150, 80, {"dragon scales", "dragon tooth", "dragon heart", "100 gold coins"})
 
-from items import Weapons
-class Player:
-    def __init__(self, name, health, weapon):
-        self.name = name
-        self.health = health
-        self.weapon = weapon
-    def take_damage(self, damage):
-        self.health -= damage
-        if self.health <= 0:
-            print(f"{self.name} has fallen in battle!")
-        else:
-            print(f"{self.name} now has {self.health} health left")
-    def attack(self, monster):
-        if self.weapon.durability > 0:
-            damage = self.weapon.damage
-            print(f"{self.name} attacks with {self.weapon.name} for {damage} damage!")
-            self.weapon.durability -= 1
-            monster.take_damage(damage)
-        else:
-            print(f"{self.weapon.name} is broken and cannot be used!")
-    def collect_drops(self, drops):
-        if drops:
-            self.inventory.extend(drops)
-            print(f"{self.name} collected the following items:")
-            for item in drops:
-                print(f"- {item}")
-        else:
-            print("No items to collect.")
-sword = Weapons("Sword", 15, 50, "N/A", "N/A", 15, "Common")
-player = Player("Arthur", 100, sword)
-werewolf.interact(player)
+werewolf.interact(MainCharacter)
+goblin.interact(MainCharacter)
+skeleton.interact(MainCharacter)
+slime.interact(MainCharacter)
+dragon.interact(MainCharacter)
